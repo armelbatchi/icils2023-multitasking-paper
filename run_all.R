f0a40b896903565605312d2c9c6c667a1af2987c
Armel Batchi <a.batchi-bouyou@wustl.edu>
2026-04-05 08:54:26 +0100

    Add files via upload

---
+++ run_all.R
@@ -0,0 +1,11 @@
+# Run the full Paper B pipeline
+
+source("R/00_setup.R")
+source("R/01_load_icils.R")
+source("R/02_icils_descriptives_and_figures.R")
+source("R/03_icils_country_models_and_forests.R")
+source("R/04_icils_secondary_analyses.R")
+source("R/05_load_timss.R")
+source("R/06_timss_main_analyses.R")
+source("R/07_cross_study_summary.R")
+source("R/08_supplementary_materials.R")
